7febf416c89a5478dcac72fb7b3202fa694a0b79
_Lawrence Garol <115449147+LaLaLandExists@users.noreply.github.com>
2025-10-14 00:25:27 +0800

    Fixed broken transitions and state flags oops.

--- eeprom_programmer/control_rom.cpp
+++ eeprom_programmer/control_rom.cpp
@@ -1,20 +1,28 @@
 
 #include "eeprom_data.h"
 
+// You don't have to clear the EEPROM if you load this as it defines the entire address space
+
+// [C]IF - Clock Inhibit Flag - stops the clock from advancing
+// [A]DF - Alarm Display Flag - switches the display to show alarm
+// [S]EF - Set Enabled Flag - enables the setting of the time shown in display
+// [D]PF - Display Pulsate Flag - enables display pulsing at 1 Hz
+// [R]DF - Red Display Flag - forces the display to show the time in red
+// [B]EF - Blare Enabled Flag - enables the buzzer
 const DataDescriptor CONTROL_ROM[] = {
   //           ABCD     CASDRB
-  { "xxxxxxxxx 0000", 0b000000 },
-  { "xxxxxxxxx 0001", 0b100110 },
-  { "xxxxxxxxx 0010", 0b101010 },
-  { "xxxxxxxxx 0011", 0b100110 },
-  { "xxxxxxxxx 1000", 0b000000 },
-  { "xxxxxxxxx 1001", 0b000110 },
-  { "xxxxxxxxx 1010", 0b011010 },
-  { "xxxxxxxxx 1011", 0b000110 },
-  { "xxxxxxxxx 1100", 0b010111 },
-  { "xxxxxxxxx 1101", 0b000110 },
-  { "xxxxxxxxx 1110", 0b000110 },
-  { "xxxxxxxxx 1111", 0b000000 },
-  
+  { "xxxxxxxxx xxxx", 0b100110 }, // At first, set everything to faulty configuration (the clock is paused, the display is red and blinking).
+  { "xxxxxxxxx 0000", 0b000000 }, // S0 - "Running"
+  { "xxxxxxxxx 0001", 0b100010 }, // S1 - "Pre-setting"
+  { "xxxxxxxxx 0010", 0b101010 }, // S2 - "Setting"
+  { "xxxxxxxxx 0011", 0b100010 }, // S3 - "Post-setting"
+  { "xxxxxxxxx 1000", 0b000000 }, // S8 - "Running w/ Alarm"
+  { "xxxxxxxxx 1001", 0b110010 }, // S9 - "Pre-alarm"
+  { "xxxxxxxxx 1010", 0b111010 }, // S10 - "Alarm Setting"
+  { "xxxxxxxxx 1011", 0b110010 }, // S11 - "Post-alarm"
+  { "xxxxxxxxx 1100", 0b000000 }, // S12 - "Abort"
+  { "xxxxxxxxx 1101", 0b010111 }, // S13 - "Blare"
+  { "xxxxxxxxx 1110", 0b000010 }, // S14 - "Repeat Alarm"
+  { "xxxxxxxxx 1111", 0b000010 }, // S15 - "Cancel Alarm"
   { nullptr, 0 }
 };
--- eeprom_programmer/ttable_rom.cpp
+++ eeprom_programmer/ttable_rom.cpp
@@ -2,25 +2,25 @@
 #include "eeprom_data.h"
 
 const DataDescriptor TTABLE_ROM[] = {
-  // Dynamic state transitions
-  //        ABCD cat
-  { "xxxxxx 0000 01x", 1 },
-  { "xxxxxx 0000 10x", 9 },
-  { "xxxxxx 0001 0xx", 2 },
-  { "xxxxxx 0010 1xx", 3 },
-  { "xxxxxx 0011 0xx", 0 },
-  { "xxxxxx 1000 001", 12 },
-  { "xxxxxx 1000 010", 15 },
-  { "xxxxxx 1000 100", 1 },
-  { "xxxxxx 1001 x0x", 10 },
-  { "xxxxxx 1010 x1x", 11 },
-  { "xxxxxx 1011 x0x", 8 },
-  { "xxxxxx 1100 01x", 13 },
-  { "xxxxxx 1100 10x", 14 },
-  { "xxxxxx 1101 x0x", 8 },
-  { "xxxxxx 1110 0xx", 0 },
-  { "xxxxxx 1111 x0x", 0 },
-  // Null state transitions
+  // State transitions
+  //       Q3210 cat
+  { "xxxxxx 0000 10x", 1  }, // S0 =cst=> S1
+  { "xxxxxx 0000 01x", 9  }, // S0 =ast=> S9
+  { "xxxxxx 0001 0xx", 2  }, // S1 =!cst=> S2
+  { "xxxxxx 0010 1xx", 3  }, // S2 =cst=> S3
+  { "xxxxxx 0011 0xx", 0  }, // S3 =!cst=> S0
+  { "xxxxxx 1000 100", 1  }, // S8 =cst=> S1
+  { "xxxxxx 1000 010", 12 }, // S8 =ast=> S12
+  { "xxxxxx 1000 001", 13 }, // S8 =att=> S13
+  { "xxxxxx 1001 x0x", 10 }, // S9 =!ast=> S10
+  { "xxxxxx 1010 x1x", 11 }, // S10 =ast=> S11
+  { "xxxxxx 1011 x0x", 8  }, // S11 =!ast=> S8
+  { "xxxxxx 1100 x0x", 0  }, // S12 =!ast=> S0
+  { "xxxxxx 1101 01x", 14 }, // S13 =ast=> S14
+  { "xxxxxx 1101 10x", 15 }, // S13 =cst=> S15
+  { "xxxxxx 1110 x0x", 8  }, // S14 =!ast=> S8
+  { "xxxxxx 1111 0xx", 0  }, // S15 =!cst=> S0
+  // Null transitions
   { "xxxxxx 0000 00x", 0 },
   { "xxxxxx 0000 11x", 0 },
   { "xxxxxx 0001 1xx", 1 },
@@ -34,11 +34,15 @@ const DataDescriptor TTABLE_ROM[] = {
   { "xxxxxx 1001 x1x", 9 },
   { "xxxxxx 1010 x0x", 10 },
   { "xxxxxx 1011 x1x", 11 },
-  { "xxxxxx 1100 00x", 12 },
-  { "xxxxxx 1100 11x", 12 },
-  { "xxxxxx 1101 x1x", 13 },
-  { "xxxxxx 1110 1xx", 14 },
-  { "xxxxxx 1111 x1x", 15 },
-
+  { "xxxxxx 1100 x1x", 12 },
+  { "xxxxxx 1101 00x", 13 },
+  { "xxxxxx 1101 11x", 13 },
+  { "xxxxxx 1110 x1x", 14 },
+  { "xxxxxx 1111 1xx", 15 },
+  // Error states, these all transition to S0
+  { "xxxxxx 0100 xxx", 0 },
+  { "xxxxxx 0101 xxx", 0 },
+  { "xxxxxx 0110 xxx", 0 },
+  { "xxxxxx 0111 xxx", 0 },
   { nullptr, 0 }
 };
